aafc5e766930096358f1b2a784288d1ea29eebd6
Nitisha <nitishamali23@gmail.com>
2026-04-20 15:32:02 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,1 +1,10 @@
+import boto3
 
+s3 = boto3.client('s3')
+
+bucket_name = 'your-bucket-name'
+file_name = 'index.html'
+
+s3.upload_file(file_name, bucket_name, file_name)
+
+print("Deployment successful 🚀")
